52907b660bcfdb83e4594aa95fb6bf9cd01d59e5
Eslam <islam.hassan@el-dokan.com>
2022-06-09 11:10:37 +0200

    Change config values

--- config/orange.php
+++ config/orange.php
@@ -8,8 +8,8 @@ return [
         'sms' => [
             'api_domain' => 'https://marketingportal.access2arabia.com:7755',
             'send_sms_api_uri' => '/JSON/API/A2A/SendSMS',
-            'sender_id' => envFromDB('ORANGE_SENDER_ID') ?? null,
-            'username' => envFromDB('ORANGE_SMS_USERNAME') ?? null,
-            'password' => envFromDB('ORANGE_SMS_PASSWORD') ?? null
+            'sender_id' => env('ORANGE_SENDER_ID') ?? null,
+            'username' => env('ORANGE_SMS_USERNAME') ?? null,
+            'password' => env('ORANGE_SMS_PASSWORD') ?? null
         ]
 ];
